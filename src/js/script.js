66f4d87be1f4655c98d4e4d111149dabaf9f09b6
Andrei Markouski <insawm@gmail.com>
2021-11-01 09:07:43 +0300

    optimized "for" cycle

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 document.querySelector('.menu-button-list').addEventListener('click', (event) => {
   if (event.target.matches('.menu-button') || event.target.matches('.tab-name')) clearActive();
 
-  for (let i = 0; i < document.querySelectorAll('.menu-button').length; i++) {
-    if (event.target.matches('.button' + (i + 1))) {
-      document.querySelector('.tab' + (i + 1)).classList.add('active');
+  for (let i = 1; i <= document.querySelectorAll('.menu-button').length; i++) {
+    if (event.target.matches('.button' + i)) {
+      document.querySelector('.tab' + i).classList.add('active');
     }
   }
 });
